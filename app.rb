427c6675d7879930ef4566e5929b0cd000973284
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-12-22 17:00:22 +0000

    Creadas páginas del sitio y enlaces en el menú

--- app.rb
+++ app.rb
@@ -5,3 +5,19 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get'/recomendados' do
+  erb :recomendados
+end
+
+get'/buscar-sitio' do
+  erb :bsitio
+end
+
+get '/buscar-enfermedad' do
+  erb :benfermedad
+end
+
+get '/buscar-beneficio' do
+  erb :bbeneficio
+end
